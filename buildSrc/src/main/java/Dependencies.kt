910e2326a7111fe7fcd7269c9917d48f5100ecf5
Jimmy <me@jimmymorales.dev>
2021-04-20 11:19:49 -0600

    test: ConsumableEventTest.kt

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -14,6 +14,7 @@ object Versions {
     const val ESPRESSO_CORE = "3.3.0"
     const val FRAGMENT = "1.3.2"
     const val JUNIT = "4.13.2"
+    const val KOTEST = "4.4.3"
     const val KTLINT = "0.40.0"
     const val TIMBER = "4.7.1"
     const val VIEWMODEL = "2.3.1"
@@ -39,6 +40,9 @@ object SupportLibs {
 
 object TestingLib {
     const val JUNIT = "junit:junit:${Versions.JUNIT}"
+    const val KOTEST_ASSERTIONS_CORE = "io.kotest:kotest-assertions-core:${Versions.KOTEST}"
+    const val KOTEST_PROPERTY_TESTING = "io.kotest:kotest-property:${Versions.KOTEST}"
+
 }
 
 object AndroidTestingLib {
