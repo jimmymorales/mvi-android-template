4edb078d180422a572e758284950235c498de9bf
Jimmy <me@jimmymorales.dev>
2021-07-14 19:48:06 -0600

    chore: Update Kotlin 1.5.0 -> 1.5.21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,12 +6,10 @@ repositories {
     google()
 }
 
-kotlinDslPluginOptions.experimentalWarning.set(false)
-
 object Plugins {
     const val AGP = "4.2.1"
     const val DOKKA = "1.4.32"
-    const val KOTLIN = "1.5.0"
+    const val KOTLIN = "1.5.21"
 }
 
 dependencies {
