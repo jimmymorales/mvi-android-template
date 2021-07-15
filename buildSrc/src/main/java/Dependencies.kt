5eb7c55393c5608713ccfe646ae8844f191ffc6e
Jimmy Morales <me@jimmymorales.dev>
2021-07-15 14:35:00 -0600

    Dependencies Maintenance (#6)

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,32 +5,32 @@ object Sdk {
 }
 
 object Versions {
-    const val ANDROIDX_TEST_EXT = "1.1.2"
-    const val ANDROIDX_TEST = "1.3.0"
+    const val ANDROIDX_TEST_EXT = "1.1.3"
+    const val ANDROIDX_TEST = "1.4.0"
     const val APPCOMPAT = "1.3.0"
     const val CONSTRAINT_LAYOUT = "2.0.4"
-    const val CORE_KTX = "1.5.0"
-    const val COROUTINES = "1.5.0"
-    const val ESPRESSO_CORE = "3.3.0"
-    const val FRAGMENT = "1.3.4"
+    const val CORE_KTX = "1.6.0"
+    const val COROUTINES = "1.5.1"
+    const val ESPRESSO_CORE = "3.4.0"
+    const val FRAGMENT = "1.3.5"
     const val JUNIT = "4.13.2"
-    const val KOTEST = "4.6.0"
+    const val KOTEST = "4.6.1"
     const val KTLINT = "0.41.0"
-    const val LIFECYCLE = "2.4.0-alpha01"
+    const val LIFECYCLE = "2.4.0-alpha02"
     const val TIMBER = "4.7.1"
-    const val TURBINE = "0.5.1"
+    const val TURBINE = "0.5.2"
 }
 
 object BuildPluginsVersion {
     const val DETEKT = "1.17.1"
-    const val KTLINT = "10.0.0"
-    const val VERSIONS_PLUGIN = "0.38.0"
+    const val KTLINT = "10.1.0"
+    const val VERSIONS_PLUGIN = "0.39.0"
 }
 
 object SupportLibs {
     const val ANDROIDX_APPCOMPAT = "androidx.appcompat:appcompat:${Versions.APPCOMPAT}"
     const val ANDROIDX_CONSTRAINT_LAYOUT =
-        "com.android.support.constraint:constraint-layout:${Versions.CONSTRAINT_LAYOUT}"
+        "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT}"
     const val ANDROIDX_CORE_KTX = "androidx.core:core-ktx:${Versions.CORE_KTX}"
     const val ANDROIDX_FRAGMENT = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT}"
     const val ANDROIDX_LIFECYCLE_RUNTIME =
@@ -48,7 +48,6 @@ object TestingLib {
     const val KOTEST_ASSERTIONS_CORE = "io.kotest:kotest-assertions-core:${Versions.KOTEST}"
     const val KOTEST_PROPERTY_TESTING = "io.kotest:kotest-property:${Versions.KOTEST}"
     const val TURBINE = "app.cash.turbine:turbine:${Versions.TURBINE}"
-
 }
 
 object AndroidTestingLib {
