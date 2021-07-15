f3f2cead3325fe14d06475cd8235866857526b2b
Jimmy <me@jimmymorales.dev>
2021-07-14 19:15:51 -0600

    chore: Update Androidx Espresso 3.3.0 -> 3.4.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -11,7 +11,7 @@ object Versions {
     const val CONSTRAINT_LAYOUT = "2.0.4"
     const val CORE_KTX = "1.6.0"
     const val COROUTINES = "1.5.0"
-    const val ESPRESSO_CORE = "3.3.0"
+    const val ESPRESSO_CORE = "3.4.0"
     const val FRAGMENT = "1.3.5"
     const val JUNIT = "4.13.2"
     const val KOTEST = "4.6.0"
