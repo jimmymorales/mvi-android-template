13fd0d7d088f82ca206e8bd905230db0571eab5c
Jimmy <me@jimmymorales.dev>
2021-07-14 19:28:46 -0600

    chore: Update Ktlint Gradle 10.0.0 -> 10.1.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -23,7 +23,7 @@ object Versions {
 
 object BuildPluginsVersion {
     const val DETEKT = "1.17.1"
-    const val KTLINT = "10.0.0"
+    const val KTLINT = "10.1.0"
     const val VERSIONS_PLUGIN = "0.38.0"
 }
 
